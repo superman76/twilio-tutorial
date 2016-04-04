7fe21b1d7d51628ca90bf1013835f7361543439f
superman76 <cbloodjr@gmail.com>
2016-04-04 17:01:41 -0600

    added ngrok, send/rec text msg is working

---
+++ server/app.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+var express = require('express');
+var app = new express.Router();
+
+app.get('/sms', function getCb(req, res) {
+	console.log(req.body);
+
+  var twiml = [
+    '<?xml version="1.0" encoding="UTF-8" ?>',
+    '<Response>',
+    '  <Message>You said: ' + req.body.Body + '</Message>',
+    '</Response>'
+  ].join('');
+
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml);
+});
+
+module.exports = app;
---
+++ sms.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+var config = require('./config');
+var twilio = require('twilio')(config.twilio.accountSid, config.twilio.authToken);
+
+twilio.messages.create({
+  body: 'hello from twilio!',
+  to: config.myPhone,
+  from: config.twilio.from,
+  mediaUrl: 'http://www.placecage.com/200/300'
+}, function createCb(err, data) {
+  if (err) return console.log('error sending message!', err);
+  return console.log('sent message', data);
+});
+
+
+
+
+
